3f2e5b8f57d0cc9ef3395b92c88134c64bb579d9
WZWren <115881359+WZWren@users.noreply.github.com>
2023-05-23 22:07:49 +0800

    Added sign out to the main screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,24 @@
 import { View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { Text, Button } from "react-native-paper";
+import { auth } from "../lib/firebase";
+import { signOut } from "firebase/auth";
+
+const handleSignout = async () => {
+    signOut(auth)
+        .then((success) => {
+            console.log("Logout successful.")
+        }).catch((error) => {
+            console.log("Error: " + error.message)
+        });
+}
 
 export default function HomeScreen() {
     console.log("Index loading...");
+
     return (
-        <View>
-            <Text>You shouldn't see this.</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', gap: 4 }}>
+            <Text>You should actually see this now :D</Text>
+            <Button onPress={ handleSignout }>Sign Out</Button>
         </View>
     );
 }
