4aeef0204da4f575d78da59fb2961ecea959a716
WZWren <115881359+WZWren@users.noreply.github.com>
2023-05-25 17:39:53 +0800

    Added toggle to lock navigation when needed

--- app/(main)/_layout.jsx
+++ app/(main)/_layout.jsx
@@ -1,5 +1,41 @@
+import { View } from "react-native";
+import { Text, Switch, Avatar } from "react-native-paper";
 import { Drawer } from "./drawer";
+import { useState } from "react";
+
+function DrawerToggle({ toggleDrawer, setToggleDrawer }) {
+    return (
+        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
+            <Text>Lock Swipe Drawer</Text>
+            <Switch value={ toggleDrawer } onValueChange={ setToggleDrawer } />
+        </View>
+    );
+}
+
+function IconReplace() {
+    return (
+        <View style={{ padding: 10 }}>
+            <Avatar.Icon size={30} icon="cctv"/>
+        </View>
+    );
+}
 
 export default function DrawerRoot() {
-  return <Drawer />;
+    const [toggleDrawer, setToggleDrawer] = useState(false);
+
+    return (
+        <Drawer
+            screenOptions={{
+                headerLeft: IconReplace,
+                headerRight: () => DrawerToggle({toggleDrawer, setToggleDrawer}),
+            }}>
+            <Drawer.Screen
+                name="main"
+                options={{
+                    drawerLabel: "Dashboard",
+                    title: "Welcome",
+                    swipeEnabled: !toggleDrawer
+                }}/>
+        </Drawer>
+    );
 }
