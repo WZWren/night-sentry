548323d0e3c239abaa4dea8d72a4981197b7e72d
WZWren <115881359+WZWren@users.noreply.github.com>
2023-07-12 16:33:03 +0800

    Added skeleton code for aggregated feed.

--- app/(main)/(feed)/dashboard.jsx
+++ app/(main)/(feed)/dashboard.jsx
@@ -3,6 +3,7 @@ import { Text, Chip, Card, IconButton } from "react-native-paper";
 import { viewStyle } from "../../../ui/style";
 import { useState } from "react";
 import { useRouter } from "expo-router";
+import { epochToDate } from "../../../lib/utils";
 
 export default function DashboardPage() {
     const router = useRouter();
@@ -34,7 +35,7 @@ export default function DashboardPage() {
                 </Chip>
             </View>
             <FlatList
-                data={["The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues.", "2", "3"]}
+                data={mockedData}
                 renderItem={({item}) => FeedCard(item, router)}
                 style={{ width: "90%" }}/>
         </View>
@@ -42,27 +43,30 @@ export default function DashboardPage() {
 }
 
 function FeedCard(item, router) {
-    const uri = item == "2" ? null : "https://spiritislandwiki.com/images/thumb/4/4e/Ember-Eyed_Behemoth.png/400px-Ember-Eyed_Behemoth.png";
-
     const handleReadMore = () => {
         router.push({
             pathname: '/focusedfeed',
             params: {
-                title: ":skull_emoji:",
+                thread_id: item.id,
+                title: item.title,
             }
         })
+    };
+
+    const handleVerify = () => {
+        console.log("Verify button pressed for card with text " + item);
     }
 
     return (
         <Card style={{ margin: 4 }}>
-            <Card.Title title="Card Title" subtitle="timestamp"/>
+            <Card.Title title={item.title} subtitle={epochToDate(item.created_at)}/>
             <Card.Content style={{ flexDirection: "row", gap: 6 }}>
-                { uri && <Image style={{ width: 100, height: 100, flex: 1 }} source={{ uri }} /> }
-                <Text numberOfLines={6} style={{ flex: 2, flexWrap: 'wrap' }}>{item}</Text>
+                { item.image && <Image style={{ width: 100, height: 100, flex: 1 }} source={{ uri: item.image }} /> }
+                <Text numberOfLines={6} style={{ flex: 2, flexWrap: 'wrap' }}>{item.desc}</Text>
             </Card.Content>
             <Card.Actions>
-                <Text variant="labelSmall" style={{ flex: 5 }}>Verification Score: {100}</Text>
-                <IconButton icon="check-decagram" style={{ flex: 1 }}/>
+                <Text variant="labelSmall" style={{ flex: 5 }}>Verification Score: {item.verify_count}</Text>
+                <IconButton onPress={handleVerify} icon="check-decagram" style={{ flex: 1 }}/>
                 <IconButton onPress={handleReadMore} icon="dots-vertical" style={{ flex: 1 }}/>
             </Card.Actions>
         </Card>
@@ -75,3 +79,43 @@ const ListTabs = Object.freeze({
     CLOSE: 2,
     ALL: 3,
 })
+
+// mock data of threads
+const mockedData = [
+    {
+        id: "1",
+        verify_count: 213,
+        title: "Crime Alert for Shoplifting near Orchard Road",
+        desc: "375 arrested since 2014, be on the lookout for suspicious individuals.",
+        created_at: 1492581612000,
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/0a/07/8f/77/orchard-road.jpg",
+        coords: {}
+    },
+    {
+        id: "2",
+        verify_count: 213,
+        title: "Fire in this location here",
+        desc: "Credit to iStockPhoto for the image.",
+        created_at: 1455555555000,
+        image: "https://media.istockphoto.com/id/507185368/photo/fire-in-a-house.jpg?s=1024x1024&w=is&k=20&c=FfOWMzgl70ujTWn-iXENhjcp52VmjA3IxKzVYbLsKoY=",
+        coords: {}
+    },
+    {
+        id: "3",
+        verify_count: 213,
+        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        desc: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        created_at: 1598781225000,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Cheese_display%2C_Cambridge_MA_-_DSC05391.jpg/1280px-Cheese_display%2C_Cambridge_MA_-_DSC05391.jpg",
+        coords: {}
+    },
+    {
+        id: "4",
+        verify_count: 4,
+        title: "Suspicious people following at night",
+        desc: "Stay safe folks, don't walk around alone at night in this area!",
+        created_at: 1681884012000,
+        image: null,
+        coords: {}
+    },
+];
--- app/(main)/(feed)/focusedfeed.jsx
+++ app/(main)/(feed)/focusedfeed.jsx
@@ -1,7 +1,12 @@
+import { useLocalSearchParams } from "expo-router";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 
 export default function FocusedFeedPage() {
+    const { thread_id } = useLocalSearchParams();
+
+    console.log(thread_id);
+
     return (
         <View>
             <Text>Test Page</Text>
