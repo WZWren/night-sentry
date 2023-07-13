c908036c4f6f8b2399529cbf5e85e27b583bbd6c
WZWren <115881359+WZWren@users.noreply.github.com>
2023-07-14 02:37:57 +0800

    Migrated to Expo 49 - see description
    In particular:
     - Expo 49 allows us to use Expo Router 2.0, which adds its own testing library that makes use of react-native libraries to perform its renders.
     - babel-jest was not properly configured in previous commits in package.json. This has been fixed.
     - The usage of Async-Storage for Jest allows us to use the relevant libraries innate mocks.
     - Expo Router 2.0 broke their recommended method to navigate using the protectedRoute function. setImmediate is used as a wrapper to workaround this issue for Android.
    
    router.test.js and provider.backup.js are still skeleton test files and do not properly test for anything yet.

--- __test__/app/router.test.js
+++ __test__/app/router.test.js
@@ -1,7 +1,41 @@
-import { screen, renderRouter } from "expo-router/testing-library";
+import { Text, View } from "react-native";
+import { screen, renderRouter } from "expo-router/src/testing-library";
+import { render } from "@testing-library/react-native";
+import { AuthProvider, useAuth } from "../../contexts/auth";
+import { supabase } from "../../lib/supabase";
+
+const TestAuthComponent = () => {
+    const { loggedIn } = useAuth();
+    return (
+        <View>
+            <Text testID="log_value">{loggedIn?.id}</Text>
+            <Text>testhere</Text>
+        </View>
+    );
+}
 
 test("render the application", async () => {
     renderRouter();
-    const signInText = await screen.findByText("Log In...");
+    const signInText = await screen.findByText("Log in...");
     expect(signInText).toBeTruthy();
+});
+
+describe("AuthProvider", () => {
+    it('provides logged-in data to the context as expected', async () => {
+        
+
+        const { getByText } = render(
+            <AuthProvider>
+                <TestAuthComponent />
+            </AuthProvider>
+        );
+
+        await supabase.auth.signInWithPassword({ email: process.env.TESTEMAIL, password: process.env.TESTPASSWORD });
+
+        const textElement = getByText("test_id_here");
+        console.log(textElement);
+        expect(textElement.props).toBeTruthy();
+
+        mock.mockRestore();
+    });
 });
